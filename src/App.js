f07253fe92350c88a190b809cda1dbb692e1de1f
Rafael Estrada <rafaelestrada@comcast.net>
2021-08-24 18:45:06 -0700

    renamed  file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
-import Sidebar  from './Sidebare';
+import Sidebar  from './Sidebar';
 
 function App() {
   return (
