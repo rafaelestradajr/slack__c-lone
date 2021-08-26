667b9dfdef8a459ab4bba4bbe447355e6e8d91ff
Rafael Estrada <rafaelestrada@comcast.net>
2021-08-25 21:49:22 -0700

    setup firebase

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,30 +1,47 @@
-import React from 'react';
-import './Sidebar.css';
-import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord'
-import CreateIcon from '@material-ui/icons/Create';
-import InsertCommentIcon from '@material-ui/icons/InsertComment';
-import SidebarOption from './SidebarOption';
-
+import React from "react";
+import "./Sidebar.css";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
+import CreateIcon from "@material-ui/icons/Create";
+import InsertCommentIcon from "@material-ui/icons/InsertComment";
+import SidebarOption from "./SidebarOption";
+import InboxIcon from "@material-ui/icons/Inbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import BookmarkBorderIcon from "@material-ui/icons/Bookmark";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
+import AppsIcon from "@material-ui/icons/Apps";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
 
 function Sidebar() {
-    return (
-        <div className='sidebar'>
-            <div className="sidebar__header">
-                <div className="sidebar__info">
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <div className="sidebar__info">
+          <h2> Slack Clone</h2>
 
-              
-                <h2> Slack Clone</h2>
-
-              <h3>
-                  <FiberManualRecordIcon/>
-                  rafael estrada
-              </h3>
-              </div>
-            <CreateIcon/>
-            </div>
-            <SidebarOption Icon={InsertCommentIcon} title='Threads'/>
+          <h3>
+            <FiberManualRecordIcon />
+            rafael estrada
+          </h3>
         </div>
-    )
+        <CreateIcon />
+      </div>
+      <SidebarOption Icon={InsertCommentIcon} title="Threads" />
+      <SidebarOption Icon={InboxIcon} title="Mention & Reactions" />
+      <SidebarOption Icon={DraftsIcon} title="Saved Items" />
+      <SidebarOption Icon={BookmarkBorderIcon} title="Channel Browser" />
+      <SidebarOption Icon={PeopleAltIcon} title="People & user groups" />
+      <SidebarOption Icon={AppsIcon} title="Apps" />
+      <SidebarOption Icon={FileCopyIcon} title="Files browser" />
+      <SidebarOption Icon={ExpandLessIcon} title="Show less" />
+      <hr />
+      <SidebarOption Icon={ExpandMoreIcon} title="Channels" />
+      <hr />
+      <SidebarOption Icon={AddIcon} addChannelOption title="Add Channel" />
+    </div>
+  );
 }
 
 export default Sidebar;
--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -5,12 +5,19 @@ function SidebarOption({Icon,title}) {
     return (
         <div className='sidebarOption'>
             {Icon && <Icon  className='sidebarOption__icon'/>}
-            {Icon ? 
-                <h3>{title}</h3> : <h3># {title}</h3>
-            }
+            {Icon ? (
+                <h3>{title}</h3> 
+                  ) : ( 
+                  <h3 className='sidebarOption__channel'>
+                        <span className='sidebarOption__hash'># </span>{title}
+                    </h3>    
+                    
+    )}
             
         </div>
-    )
-}
+           );
+           }
+    
 
-export default SidebarOption
+
+export default SidebarOption;
